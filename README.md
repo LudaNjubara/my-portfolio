00190d0ed7cf1ff0d637b2d6e6fdf0a851ed3a21
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-04 00:51:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Portfolio
+
+Check out my portfolio! It's got all my frontend and full-stack work in one place. Get to know me and my skills as a web developer. Let's go!
---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "My portofolio",
+  description: "Porfolio where you can view my projects and learn more about me!",
+  authors: [
+    {
+      name: "Mihael Šestak",
+      url: "https://www.linkedin.com/in/mihael-sestak-b73590241/",
+    },
+  ],
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
