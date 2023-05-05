3c8f91b360b7c8cb62cf927a34df99b61cb2f423
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-06 01:12:39 +0200

    Added constants for page content

---
+++ constants/pageContent.tsx
@@ -0,0 +1,43 @@
+export default {
+  global: {
+    firstName: "Mihael",
+    lastName: "Šestak",
+    email: "mihael.sestak00@gmail.com",
+    phoneNumber: "+385 97 721 2003",
+    location: "Zagreb, Croatia",
+    social: {
+      github: "https://www.github.com/LudaNjubara",
+      linkedin: "https://www.linkedin.com/in/mihael-sestak-b73590241/",
+      instagram: "https://www.instagram.com/mihael_sestak/",
+    },
+  },
+  heroSection: {
+    title: "Hey there, I'm",
+    subtitle: "Frontend developer",
+    description:
+      "I'm a frontend developer with a passion for building beautiful and functional websites and applications. I have experience with backend as well and have built web apps utilizing my full stack skills. I'm currently looking for a new role in an exciting company.",
+    cards: {
+      services: {
+        title: "My Services",
+        description:
+          "I like to think my services are diverse and cover a wide range of needs. I'm always looking to expand my skillset and learn new things. Click or tap to view them all.",
+      },
+      projects: {
+        title: "My Projects",
+        description:
+          "Explore my projects and see what I've been working on. There's a little bit of everything. I always tend to work on things that bring me value. I hope you find value in them too. Feel free to check them out.",
+      },
+      contact: {
+        title: "Get in touch",
+        description:
+          "If you'd like to get in touch with me, feel free to send me a message. I'll try to get back to you as soon as possible.",
+      },
+    },
+  },
+
+  aboutMeSection: {
+    title: "About me",
+    description:
+      'In my school days I tended to sit in the back of the class, away from the spotlights where all the "fun" was happening, and yet here I am as a frontend dev... I guess life really knows how to come full circle... Anyways, I digress. I like to spend my time learning and building projects. Some small some large, some make it to production some dont, but the important part of it all is figuring out those little tricks that arent exactly Google-able, like building my intuition and learning how to learn. Im a big fan of the "learn by doing" approach. I guess Im a big fan of learning in general haha. What Im also a big fan of is sharing what Ive learned, so I hope you find some value in my content as well.',
+  },
+};
