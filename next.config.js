9779efa336bd33efd4691831f8ea99e019d769d2
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-06 01:08:43 +0200

    Added image domains to next.config; added helpful paths to tsconfig for easier react imports

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ["images.unsplash.com", "st2.depositphotos.com", "mir-s3-cdn-cf.behance.net"],
+  },
+};
+
+module.exports = nextConfig;
