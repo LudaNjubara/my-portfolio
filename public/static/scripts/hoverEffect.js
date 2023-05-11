14375ff0eafe9d2b16a5a31ee1cdc4dc56b701ac
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-11 02:37:54 +0200

    Refactored code for better clarity and maintainance

--- public/static/scripts/hoverEffect.js
+++ public/static/scripts/hoverEffect.js
@@ -1,14 +1,23 @@
-/* Get all cards that contain "card" in card-ghjfr7 */
 const cards = document.querySelectorAll("[class*=heroSection_card__EJCL2]");
 const cardsContainer = document.querySelector(".heroSection_heroSection__content__rU8lS");
 
-cardsContainer.onmousemove = (e) => {
-  for (const card of cards) {
-    const rect = card.getBoundingClientRect();
-    const offsetX = e.clientX - rect.left;
-    const offsetY = e.clientY - rect.top;
+function setCardOffset(card, x, y) {
+  card.style.setProperty("--offsetX", `${x}px`);
+  card.style.setProperty("--offsetY", `${y}px`);
+}
 
-    card.style.setProperty("--offsetX", `${offsetX}px`);
-    card.style.setProperty("--offsetY", `${offsetY}px`);
-  }
+function getMouseOffset(card, event) {
+  const rect = card.getBoundingClientRect();
+  const offsetX = event.clientX - rect.left;
+  const offsetY = event.clientY - rect.top;
+  return { offsetX, offsetY };
+}
+
+window.onload = () => {
+  cardsContainer.onmousemove = (e) => {
+    for (const card of cards) {
+      const { offsetX, offsetY } = getMouseOffset(card, e);
+      setCardOffset(card, offsetX, offsetY);
+    }
+  };
 };
