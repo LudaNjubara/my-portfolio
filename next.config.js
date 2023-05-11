c33aa64f679c335540468eec8ecfba43b510c0ac
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-11 03:40:02 +0200

    Removed unused image domains

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-
-  images: {
-    domains: ["images.unsplash.com", "st2.depositphotos.com", "mir-s3-cdn-cf.behance.net"],
-  },
 };
 
 module.exports = nextConfig;
